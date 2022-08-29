b1a270ce14516678ae9640f2fec90f776950deb3
Alex Wilkinson <mrwilkinson1@gmail.com>
2022-08-28 22:21:13 -0400

    navbar

---
+++ assets/alexportfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PortNavbar from './components/PortNavbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PortNavbar/>
+    </div>
+  );
+}
+
+export default App;
